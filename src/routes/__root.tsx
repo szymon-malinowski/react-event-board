9693b541c8022bc9e0821119aef7b8a2cbc47f90
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-06-25 11:34:23 +0200

    refactor: remove unused ClerkHeader import from RootLayout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Navbar from "../components/organisms/Navbar";
-import ClerkHeader from "../components/organisms/ClerkHeader";
 
 const RootLayout = () => (
   <>
